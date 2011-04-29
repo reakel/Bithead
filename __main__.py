2c9c68219ca2264098e156976d319c03ac7f115b
Espen Stene Brønstad <espensb@nt.ntnu.no>
2011-04-29 16:03:07 +0200

    Restructured

--- __main__.py
+++ __main__.py
@@ -9,13 +9,7 @@ from clienthandler import ClientHandler
 
 from database import Database
 
-handlers = [ 'realog',
-	     'newuser',
-	     'postinstall',
-	     'pubkey']
-
-
-configFiles = [u'/etc/bithead.conf']
+configFiles = ['/etc/bithead.conf']
 config = ConfigParser()
 config.read(configFiles)
 
@@ -24,18 +18,29 @@ Database.loadConfig(config)
 
 handlerClasses = {}
 
-for handler in handlers:
+i = 0
+for item in config.items('handlers'):
+    handler, active = item
+    active = bool(int(active))
+    if not active:
+	print 'Skipped module:', handler
+	continue
     c = handler.capitalize()
-    print handler, c
+    print 'Loading module:', handler
     exec "from clienthandler.%s import %s" % (handler, c)
     handlerClasses[handler] = eval(c)
     handlerClasses[handler].loadConfig(config)
+    i = i+1
+
+print 'Successfully loaded %i modules' % (i)
+
+
 
 
 class HTTPException(Exception):
     pass
 
-class MyHandler(BaseHTTPRequestHandler):
+class MyRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
 	try:
 	    path = self.path
@@ -49,20 +54,22 @@ class MyHandler(BaseHTTPRequestHandler):
 	    response = handler.getResponse()
 	    if not 'status' in response.keys(): 
 		response['status'] = '0'
-	    self.send_response(200)
-	    self.send_header('Content-type', 'text/html')
-	    self.end_headers()
+	    self.send_default_response()
 	    self.wfile.write(json.dumps(response))
 	    return
 	except HTTPException:
 	    self.send_error(404,'DIE')
 	except ClientHandler.Error as (errno, errstr):
-	    self.send_response(200)
-	    self.send_header('Content-type', 'text/html')
-	    self.end_headers()
+	    self.send_default_response()
 	    self.wfile.write(json.dumps({'status':errno}))
 	except Exception:
 	    self.send_error(404,'DIE')
+    
+    def send_default_response(self):
+	self.send_response(200)
+	self.send_header('Cache-Control', 'no-cache, must-revalidate')
+	self.send_header('Content-type', 'text/json')
+	self.end_headers()
 
     def do_POST(self):
 	self.send_error(404, 'DIE')
@@ -86,22 +93,11 @@ class MyHandler(BaseHTTPRequestHandler):
 	    ret[key] = value
 	return ret
 
-
-
-
-
-
-
-
-def main():
-    
+if __name__=='__main__':
     try:
-	server = HTTPServer(('',port), MyHandler)
+	server = HTTPServer(('',port), MyRequestHandler)
 	print "Started HTTPServer, listening to port %s" % (port)
 	server.serve_forever()
     except KeyboardInterrupt:
 	print 'TERM signal recieved, shutting down server'
 	server.socket.close()
-
-if __name__=='__main__':
-    main()
