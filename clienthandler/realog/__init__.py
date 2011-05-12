1d76d679c5c10d87c0dad33abab0e93e2bab6749
Espen Stene Brønstad <espensb@nt.ntnu.no>
2011-05-12 21:16:33 +0200

    Started writing realog

--- clienthandler/realog/__init__.py
+++ clienthandler/realog/__init__.py
@@ -2,4 +2,22 @@
 from clienthandler import ClientHandler
 
 class Realog(ClientHandler):
-    pass
+    def getRespone(self):
+	#Kjøre getResponse i parent klassen
+        super(Pubkey,self).getResponse()
+	#Initialisere db objekt
+        db = db.getCursor()
+	# Tar vi i mot argumenter som dict
+	args = self.args
+
+	maskinnavn = args["maskinnavn"]
+	brukernavn = args["brukernavn"]
+	innloggingstid = args["innloggingstid"]
+	utloggingstid = args["utloggingstid"]
+	# Nåværende tid kan kanskje brukes i framtiden for å sikre mot nedtid på serveren
+
+	db.query("""INSERT INTO Comp_usage(CompID, User, LoginTime, LogoutTime)
+			VALUES(%s, %s, %s, %s)
+			UPDATE Computers SET OS='ubuntu' WHERE CompID LIKE %s""" %(maskinavn, brukernavn, innloggingstid, utloggingstid, maskinnavn))
+
+        # Skal returnere dict
