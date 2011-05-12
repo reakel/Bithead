1c7456f2f16f48c68addf622f1d41ce26bc651c5
Erlend Espeland <erlendespeland@gmail.com>
2011-05-12 20:27:55 +0200

    Fiksa på newuser

--- clienthandler/newuser/__init__.py
+++ clienthandler/newuser/__init__.py
@@ -1,3 +1,9 @@
 from clienthandler import ClientHandler
 class Newuser(ClientHandler):
-    pass
+
+    def getResponse(self):
+        super(NewUser, self).getResponse()
+
+    def authUser(self, user)
+#SSHe til maskina, autentisere bruker og hente ut uid og gid.
+        return True
