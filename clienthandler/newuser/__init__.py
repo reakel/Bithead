f08d9df4dd93ae5881239b55a7eb04a186655af5
Erlend Espeland <erlendespeland@gmail.com>
2011-05-20 17:35:45 +0200

    Skrevet flere deler av newuser

--- clienthandler/newuser/__init__.py
+++ clienthandler/newuser/__init__.py
@@ -1,9 +1,27 @@
 from clienthandler import ClientHandler
+import subprocess
 class Newuser(ClientHandler):
 
     def getResponse(self):
         super(NewUser, self).getResponse()
+        user = self.args['user']
+        userdir = "/storage/ubuntu10-profiles/" + user
+        if authUser():
+            try:
+                cmd = "ssh root@%s -o StrictHostKeyChecking=no id %s" (self.args, user)
+                sshreturn = subprocess.check_output(cmd)
+            except CalledProcessError as e:
+                raise ClientHandler.Error(errno, Error when tryin to ssh to client: e.returncode e.output)
 
+            #Creates users home directory 
+            if subprocess.check_call(["mkdir", userdir])
+                raise ClientHandler.Error(200, "mkdir error")
+            #Changes owner and group of users home directory
+            if subprocess.check_call(["chown", idnums, userdir])
+                raise ClientHandler.Error(200, "chown error")
+            #Sets permissions for users home directory
+            if subprocess.check_call(["chmod", "700", userdir])
+                raise ClientHandler.Error(200, "chmod error")
     def authUser(self, user)
 #SSHe til maskina, autentisere bruker og hente ut uid og gid.
         return True
