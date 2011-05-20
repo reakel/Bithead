6f4382ec3b730b955ef5e1eafce4003e1c991285
Espen Stene Brønstad <espensb@nt.ntnu.no>
2011-05-20 12:34:56 +0200

    Added template for ClientHandlers

---
+++ clienthandler/template.py
@@ -0,0 +1,28 @@
+exit() #remove this line
+
+class HandlerTemplate(ClientHandler):
+    def getResponse(self): i
+	#handles client request and returns a client response as a dict 
+	#for time consuming processes use doPostProcessing()
+	super(ClientHandler,self).getResponse() #this will print the request arguments to the log
+	#self.args (dict): arguments from client
+	#self.addr (string): client address
+	#self.db (Database): database object
+	#   use c = self.db.getCursor() to get a cursor (google MySQLdb)
+	#self.printLog(str): print messages to log
+
+	#handle client request here
+	ret = {} #this is the response returned to client (dict)
+	#ret['status'] defined and !=0 is interpreted by the client as a request failure 
+	#best practice: raise ClientHandler.Error(errno, errmsg) (Exeption) on request failure
+
+
+	return ret
+
+    def doPostProcessing(self):
+	pass
+	#continue request handling after sending response
+	#do not return anything
+
+
+
