3fecb1cc6d4d9fec9297cc039fdf4144cbdd7fbd
Erlend Espeland <erlendespeland@gmail.com>
2011-05-26 13:22:28 +0200

    Added getConfig and minor fixes

--- clienthandler/newuser/__init__.py
+++ clienthandler/newuser/__init__.py
@@ -6,18 +6,20 @@ class Newuser(ClientHandler):
     def getResponse(self):
         super(NewUser, self).getResponse()
         user = self.args['user']
-        userdir = "/storage/ubuntu10-profiles/" + user
+        userdir = userdirs + user
         if not authUser():
             raise ClientHandler.Error(200, "User is not authorized!")
     try:
-	cmd = "ssh root@%s -o StrictHostKeyChecking=no id %s" % (self.args, user)
+	cmd = "ssh root@%s -o StrictHostKeyChecking=no id %s" % (self.addr, user)
 	sshreturn = subprocess.check_output(cmd.split(" "))
     except CalledProcessError as e:
         errmsg = "Error when tryin to ssh to client:" + e.returncode + e.output
 	raise ClientHandler.Error(200, errmsg)
     
     #Use regexp to get userid and groupid from output from ssh
-    m = re.match("uid=(\d+)\D+?gid=(\d+)", sshreturn)
+    m = re.search(r"uid=(\d+)\D+?gid=(\d+)", sshreturn)
+    if m is None:
+        raise ClientHandler.Error(200, "Id not found through ssh")
     uid = m.groups(0)
     gid = m.groups(1)
     idmuns = uid +":"+gid
@@ -34,6 +36,9 @@ class Newuser(ClientHandler):
     
     return {"status": 0}
 
+    def loadConfig(config):
+        section = "newuser"
+        Newuser.userdirs = config.get(section, "userdirs")
 
     def authUser(self, user)
 #SSHe til maskina, autentisere bruker og hente ut uid og gid.
